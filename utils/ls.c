fffc012a19ef9a82ce5f809a8bbf55beb2b10671
PatricioIribarneCatella <patricio@patricio-Lenovo-V360>
2017-08-28 17:20:11 -0300

    change readdir_r for readdir and stat for fstatat

--- utils/ls.c
+++ utils/ls.c
@@ -1,29 +1,28 @@
-#define _POSIX_C_SOURCE 1
-#define _GNU_SOURCE
+#define _POSIX_C_SOURCE 200809L
 
 #include <stdio.h>
-#include <string.h>
 #include <errno.h>
 #include <dirent.h>
 #include <unistd.h>
+#include <fcntl.h>
 #include <sys/types.h>
 #include <sys/stat.h>
-
-#define FILESIZE 200
 
 static void ls(const char* path) {
 
 	DIR* d;
 	struct stat st;
-	struct dirent entry;
-	struct dirent* result;
-	char buf[10];
-	char filepath[FILESIZE];
-	int r;
+	struct dirent* entry;
+	int fd;
 
-	if (stat(path, &st) < 0) {
-		fprintf(stderr, "cannot stat directory/file: %s. error: %s\n",
-			path, strerror_r(errno, buf, sizeof(buf)));
+	if ((fd = open(path, O_RDONLY)) < 0) {
+		perror("cannot open");
+		return;
+	}
+
+	if (fstat(fd, &st) < 0) {
+		perror("cannot stat");
+		close(fd);
 		return;
 	}
 
@@ -31,40 +30,37 @@ static void ls(const char* path) {
 	if (S_ISREG(st.st_mode)) {
 		printf("mode: %lo size: %ld  uid: %d  gid: %d inode: %lu file: %s\n",
 			(unsigned long)st.st_mode, st.st_size, st.st_uid, st.st_gid, st.st_ino, path);
+		close(fd);
 		return;
 	}
 	
 	// It´s a directory
 	if (S_ISDIR(st.st_mode)) {
 
-		if (!(d = opendir(path))) {
-			fprintf(stderr, "cannot open directory: %s. error: %s\n",
-				path, strerror_r(errno, buf, sizeof(buf)));
+		if (!(d = fdopendir(fd))) {
+			perror("cannot open dir");
+			close(fd);
 			return;
 		}
 
-		r = readdir_r(d, &entry, &result);
-		
-		while ((r == 0) && (result)) {
-			
-			memset(filepath, 0, sizeof(filepath));
-			strcpy(filepath, path);
-			strcat(filepath, "/");
-			strcat(filepath, entry.d_name);
+		errno = 0;
 
-			if (stat(filepath, &st) < 0) {
-				fprintf(stderr, "cannot stat directory/file: %s. error: %s\n",
-					filepath, strerror_r(errno, buf, sizeof(buf)));
+		while ((entry = readdir(d))) {
+			
+			if (fstatat(fd, entry->d_name, &st, AT_SYMLINK_NOFOLLOW) < 0) {
+				perror("cannot stat");
 			} else {
 				printf("mode: %lo size: %ld  uid: %d  gid: %d inode: %lu file: %s\n",
 					(unsigned long)st.st_mode, st.st_size, st.st_uid,
-					st.st_gid, st.st_ino, entry.d_name);
+					st.st_gid, st.st_ino, entry->d_name);
 			}
-
-			r = readdir_r(d, &entry, &result);
 		}
 
+		if (errno)
+			perror("cannot readdir");
+
 		closedir(d);
+		close(fd);
 	}
 }
 
