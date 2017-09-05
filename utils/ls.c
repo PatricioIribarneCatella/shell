a3bd32e3b5ac54391c149f190336a34fca13ecbf
PatricioIribarneCatella <patricio@patricio-Lenovo-V360>
2017-09-05 11:33:13 -0300

    Changes to ls done. Comentaries added for detailed explanations on how to detect an error on readdir. Also, it was changed how to detect if it is a regular file o somethinf else, or a directory

--- utils/ls.c
+++ utils/ls.c
@@ -26,42 +26,47 @@ static void ls(const char* path) {
 		return;
 	}
 
-	// It´s a regular file
-	if (S_ISREG(st.st_mode)) {
+	// It's not a directory (can be a symlink, a socket, a FIFO, a regular file, etc)
+	if (!S_ISDIR(st.st_mode)) {
 		printf("mode: %lo size: %ld  uid: %d  gid: %d inode: %lu file: %s\n",
 			(unsigned long)st.st_mode, st.st_size, st.st_uid, st.st_gid, st.st_ino, path);
 		close(fd);
 		return;
-	}
+	}	
 	
-	// It´s a directory
-	if (S_ISDIR(st.st_mode)) {
-
-		if (!(d = fdopendir(fd))) {
-			perror("cannot open dir");
-			close(fd);
-			return;
-		}
-
-		errno = 0;
-
-		while ((entry = readdir(d))) {
-			
-			if (fstatat(fd, entry->d_name, &st, AT_SYMLINK_NOFOLLOW) < 0) {
-				perror("cannot stat");
-			} else {
-				printf("mode: %lo size: %ld  uid: %d  gid: %d inode: %lu file: %s\n",
-					(unsigned long)st.st_mode, st.st_size, st.st_uid,
-					st.st_gid, st.st_ino, entry->d_name);
-			}
-		}
-
-		if (errno)
-			perror("cannot readdir");
-
-		closedir(d);
+	// It's a directory...
+	if (!(d = fdopendir(fd))) {
+		perror("cannot open dir");
 		close(fd);
+		return;
 	}
+
+	// errno is set to zero to check if readdir return NULL
+	// because the end of the stream was reached or an error ocurred.
+	// If it was an error, readdir returns NULL and sets errno to the
+	// corresponding number of error, otherwise also NULL is returned 
+	// but the value of errno is not changed.
+	errno = 0;
+	
+	// readdir returns the next directory entry (entry) in the directory stream (d) 
+	// or NULL if it is at the end of d
+	while ((entry = readdir(d))) {
+		
+		if (fstatat(fd, entry->d_name, &st, AT_SYMLINK_NOFOLLOW) < 0) {
+			perror("cannot stat");
+		} else {
+			printf("mode: %lo size: %ld  uid: %d  gid: %d inode: %lu file: %s\n",
+				(unsigned long)st.st_mode, st.st_size, st.st_uid,
+				st.st_gid, st.st_ino, entry->d_name);
+		}
+	}
+
+	// If it is not zero an error ocurred
+	if (errno)
+		perror("cannot readdir");
+
+	closedir(d);
+	close(fd);
 }
 
 int main(int argc, char const* argv[]) {
