66ed0c20cacb4ac7375001bcacf19055b93594f8
PatricioIribarneCatella <patricio_14_94@hotmail.com>
2018-03-17 17:58:32 -0300

    Some comments added

--- sh.c
+++ sh.c
@@ -190,11 +190,15 @@ static char* read_line(const char* promt) {
 		c = getchar();
 	}
 
+	// signal handler sets EOF in the
+	// 'stdin'
 	if (c == EOF && background) {
 		background = 0;
 		return buffer;
 	}
 	
+	// if the user press ctrl+D
+	// just exit normally
 	if (c == EOF && !background)
 		return NULL;
 
