84a2782869ec323f0e44050930bf09c1aa38f5ff
PatricioIribarneCatella <patricio_14_94@hotmail.com>
2018-04-11 19:00:03 -0300

    piping print status

--- printstatus.c
+++ printstatus.c
@@ -1,10 +1,38 @@
 #include "printstatus.h"
 
+static void print_pipe_info(struct cmd* c) {
+
+	struct pipecmd* p;
+
+	p = (struct pipecmd*)c;
+
+	if (c->type == PIPE) {
+		
+		print_pipe_info(p->leftcmd);
+
+		printf("%s", "|");
+
+		print_pipe_info(p->rightcmd);
+
+		return;
+	}
+	
+	fprintf(stdout, " %s ", c->scmd);
+
+	return;
+}
+
 // prints information of process' status
 void print_status_info(struct cmd* cmd) {
-	
+
 	if (strlen(cmd->scmd) == 0)
 		return;
+	
+	if (cmd->type == PIPE) {
+		printf("%s", "Command: [");
+		print_pipe_info(cmd);
+		printf("%s\n", "] done");
+	}
 
 	if (WIFEXITED(status)) {
 
